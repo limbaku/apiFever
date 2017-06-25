3629e1915a443da4ee03c48ccd1536617d36488a
sperez <sandraperezgalisteo"gmail.com>
2017-06-25 16:23:52 +0100

    adding security

--- src/main/java/apiFever/Application.java
+++ src/main/java/apiFever/Application.java
@@ -2,11 +2,23 @@ package apiFever;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 @SpringBootApplication
-public class Application {
+public class Application extends WebSecurityConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/connect/facebook").permitAll()
+                .anyRequest().authenticated();
+    }
 }
