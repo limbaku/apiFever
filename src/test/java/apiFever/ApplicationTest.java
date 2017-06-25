7008e594e9f41d3821d1abed23cf627bf5040e52
sperez <sandraperezgalisteo"gmail.com>
2017-06-25 22:10:56 +0100

    adding tests and readme file

---
+++ src/test/java/apiFever/ApplicationTest.java
@@ -0,0 +1,34 @@
+package apiFever;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ApplicationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void homePageRedirects() throws Exception {
+        mockMvc.perform(get("/login")).andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("https://www.facebook.com/**"));
+        ;
+    }
+
+}
